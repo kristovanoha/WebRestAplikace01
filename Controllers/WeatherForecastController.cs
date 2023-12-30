3139a2814b0b778f603700d5befbcf1b29370b69
Jiri Hegar <jiri.hegar@trustlab.cz>
2023-12-30 21:11:38 +0100

    prvni commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebRest01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("first", Name = "GetFirst")]
+        public ActionResult<string> First()
+        {
+            return "je to tady PRVNI";
+        }
+
+
+
+        [HttpGet("druha", Name = "GetDruhDDa")]
+        public ActionResult<string> Second()
+        {
+            return "tohle je druha !!!!!!!!!!!!!!!!!";
+        }
+    }
+}
